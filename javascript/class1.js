d5e4e5fc1eb2f8eaeeeec8b248abfe93debc7544
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-04-16 11:07:30 +0500

    javascript class 1

---
+++ javascript/class1.js
@@ -0,0 +1,49 @@
+// programming interface
+// hoisting let var const
+// declaration
+let a;
+//  'A' (char) (string)
+// "inititialization" (string)
+a = 5;
+
+a = 2;
+
+const b=2;
+let sum = a + b;
+
+document.write(b);
+console.log(a)
+// camel case  
+//  
+let h1=document.getElementById("abc");
+// concatination (+)
+// let output="SUM is : " + sum;
+
+// string template 
+let output=`SUM is : ${sum}`;
+
+
+h1.innerHTML=output ;
+
+// javascript with html DOM BOM
+// Find the area of a rectangle where the length is 5 and the width is 8.
+
+let w = 8;
+let l=5;
+
+
+let area = l * w;
+
+let html=`AREA OF RECTANGLE IS: &nbsp; ${area}`;
+
+h1.innerHTML = html;
+
+
+// Find the area of a triangle where the base is 4 and the height is 3.
+// Find the area of a circle where the radius is 3.
+// Convert temperatures from Celsius to Fahrenheit and Fahrenheit to Celsius.
+
+
+
+
+// ============ project ============
