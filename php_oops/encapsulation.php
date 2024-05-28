c3c22e59732cee9229236f262717c2231c345dfc
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-05-28 11:08:57 +0500

    encapsulation php oops

---
+++ php_oops/encapsulation.php
@@ -0,0 +1,96 @@
+<?php
+declare(strict_types=1);
+// oops 
+
+// ================object oreinted programming===============
+/**
+ * 1. encapsulation
+ * 2. inheritance
+ * 3. abstraction
+ * 4. polymorphism
+ 
+ 
+ 
+ */
+
+// 1.	Find the area of a rectangle where the length is 5 and the width is 8.
+// 2.	Find the area of a triangle where the base is 4 and the height is 3.
+// 3.	Find the area of a circle where the radius is 3.
+// 4.	Convert temperatures from Celsius to Fahrenheit and Fahrenheit to Celsius.
+
+
+class AreaOfRectangle
+{
+ private $length;
+ private $width;
+ private $area;
+
+
+ public function Setlength(float $l)
+ {
+  $this->length = $l;
+ }
+
+ public function SetWidth(float $w)
+ {
+  $this->width = $w;
+ }
+
+
+ public function calculate(): float|int
+ {
+  $this->area = $this->length * $this->width;
+
+  return $this->area;
+ }
+
+
+}
+
+
+
+
+
+class car
+{
+
+ private $name;
+ private $age;
+
+
+
+ public function SetName(string $a)
+ {
+  $this->name = $a;
+ }
+
+ public function GetName(): string
+ {
+  return $this->name;
+ }
+
+}
+
+
+$obj = new car;
+
+$obj->SetName("BMW");
+
+echo $obj->GetName();
+echo "<br><hr>";
+// =================================================
+
+$obj2 = new car;
+$obj2->SetName("BMW 2");
+
+echo $obj2->GetName();
+echo "<br><hr>";
+// =================================================
+
+$abc= new AreaOfRectangle;
+
+$abc->Setlength(8);
+$abc->SetWidth(5);
+
+echo $abc->calculate();
+?>
